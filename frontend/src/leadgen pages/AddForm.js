78599bc5897a8df67c064e3a8ac77dc33ab4f1bb
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-24 15:58:25 +0800

    Create AddForm.js

---
+++ frontend/src/leadgen pages/AddForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LeadForm from '../leadgen components/LeadForm'
+
+const AddForm = () => {
+    return (
+        <div className="AddForm">
+            <LeadForm />
+        </div>
+    );
+}
+
+export default AddForm
