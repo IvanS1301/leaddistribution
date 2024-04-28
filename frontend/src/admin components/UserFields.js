1d5607bd87c60158a882089389ef4582b9c5dd7d
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-28 20:36:20 +0800

    Add files via upload

---
+++ frontend/src/admin components/UserFields.js
@@ -0,0 +1,23 @@
+const UserFields = ({ lead }) => {
+
+    return (
+      <div className="lead-table">
+        <table border={0} cellSpacing={0}>
+        <thead>
+          <tr>
+          <th scope="col"><strong>ID</strong></th>
+            <th scope="col"><strong>Name</strong></th>
+            <th scope="col"><strong>Role</strong></th>
+            <th scope="col"><strong>Birthday</strong></th>
+            <th scope="col"><strong>Phone Number</strong></th>
+            <th scope="col"><strong>Email</strong></th>
+            <th scope="col"><strong>Address</strong></th>
+            <th scope="col"><strong>Actions</strong></th>
+            </tr>
+            </thead>
+        </table>
+      </div>
+    )
+  }
+  
+  export default UserFields
