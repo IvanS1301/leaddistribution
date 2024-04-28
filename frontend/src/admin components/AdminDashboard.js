d803577af99764eb6709b90bbe7e817b074fc0cf
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-28 20:42:56 +0800

    Add files via upload

---
+++ frontend/src/admin components/AdminDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../admin components/Sidebar';
+const AdminDashboard = () => {
+    return (
+        <div className="TLDashboard">
+            <Sidebar />
+        </div>
+    );
+}
+
+export default AdminDashboard
