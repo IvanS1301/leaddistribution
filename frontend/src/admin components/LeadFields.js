5d053900de9018cf4258eb168f5c625d6a06a54b
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-04-28 20:35:29 +0800

    new

---
+++ frontend/src/admin components/LeadFields.js
@@ -0,0 +1,24 @@
+const LeadFields = ({ lead }) => {
+
+    return (
+      <div className="lead-table">
+        <table border={0} cellSpacing={0}>
+        <thead>
+          <tr>
+          <th scope="col"><strong>ID</strong></th>
+            <th scope="col"><strong>Name</strong></th>
+            <th scope="col"><strong>Type</strong></th>
+            <th scope="col"><strong>Email Address</strong></th>
+            <th scope="col"><strong>Lead Gen Date</strong></th>
+            <th scope="col"><strong>Last Visit</strong></th>
+            <th scope="col"><strong>Call Disposition</strong></th>
+            <th scope="col"><strong>Assigned To</strong></th>
+            <th scope="col"><strong>Actions</strong></th>
+            </tr>
+            </thead>
+        </table>
+      </div>
+    )
+  }
+  
+  export default LeadFields
