fa845150c2797689eda346e7d090f5cba25350a8
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-05-20 19:09:54 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './global.css'
+import './global.css' // Change mo into index.css if want mo mapunta sa old design
 import App from './App'
 import { LeadsContextProvider } from './context/LeadsContext'
 import { AuthContextProvider } from './context/AuthContext'
