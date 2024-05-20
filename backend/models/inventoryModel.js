947907eef18a8dd8240f7addfff565018648b31c
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-05-20 18:13:50 +0800

    Add files via upload

---
+++ backend/models/inventoryModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const inventorySchema = new mongoose.Schema({
+    numberOfLeads: {
+        type: String
+    },
+    numberOfUsers: {
+        type: String
+    },
+    numberOfAssignedLeads: {
+        type: String
+    },
+    numberOfUnassignedLeads: {
+        type: String
+    },
+    typeCounts: {
+        type: Map,
+        of: Number,
+        required: true
+    },
+    callDispositionCounts: {
+        type: Map,
+        of: Number,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+},
+    { timestamps: { updatedAt: true } }
+);
+
+module.exports = mongoose.model('Inventory', inventorySchema)
