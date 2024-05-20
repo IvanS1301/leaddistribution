d86897d613e504236aabeaba20b331cab9668ad0
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-05-20 19:05:55 +0800

    Create EditUserInfo.jsx

---
+++ frontend/src/pages/profile/EditUserInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UpdateUserForm from '../../components/profile/UpdateUserForm'
+
+const EditUserInfo = () => {
+    return (
+        <div className="EditForm">
+            <UpdateUserForm />
+        </div>
+    );
+}
+
+export default EditUserInfo
