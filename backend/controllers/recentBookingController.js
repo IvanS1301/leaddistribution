7b0f49ce31828ee8ac6afd0c60d85a9bcf05d2dc
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-06-18 16:42:47 +0800

    Update recentBookingController.js

--- backend/controllers/recentBookingController.js
+++ backend/controllers/recentBookingController.js
@@ -2,7 +2,7 @@ const RecentBooking = require('../models/recentBookingModel')
 
 const getRecentBookings = async (req, res) => {
     try {
-        const recentBookings = await RecentBooking.find().populate('lead')
+        const recentBookings = await RecentBooking.find().populate('lead').sort({ createdAt: -1 })
         res.status(200).json(recentBookings)
     } catch (error) {
         console.error('Error fetching recent bookings:', error)
