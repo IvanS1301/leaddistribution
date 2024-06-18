4fa0c130640b1cc099a42599b3223dad4c6a614e
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-06-18 16:58:15 +0800

    Update UserLists.jsx

--- frontend/src/components/admin/UserLists.jsx
+++ frontend/src/components/admin/UserLists.jsx
@@ -1,27 +1,91 @@
-import { useEffect, useMemo } from "react"
-import { useUsersContext } from "../../hooks/useUsersContext"
-
-// mui
-import { Box, Typography } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
+import { Box, IconButton, Modal, CircularProgress, Button, Snackbar } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Delete, Visibility, Edit } from '@mui/icons-material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React, { useState } from 'react'
+import { useUsersContext } from "../../hooks/useUsersContext";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import Header from '../Chart/Header';
 import moment from 'moment'
+import EditUserInfo from '../../pages/profile/EditUserInfo';
+import ReadUserInfo from '../../pages/profile/ReadUserInfo';
 
+const UserLists = ({ userlgs, onUserUpdate }) => {
+  const { dispatch } = useUsersContext();
+  const { userLG } = useAuthContext();
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [openAssignModal, setOpenAssignModal] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [loadingDelete, setLoadingDelete] = useState(false); // State for delete loading
+  const [errorDelete, setErrorDelete] = useState(null); // State for delete error
+  const [showConfirmation, setShowConfirmation] = useState(false); // State for showing delete confirmation dialog
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // State for Snackbar open
+  const [snackbarMessage, setSnackbarMessage] = useState(""); // State for Snackbar message
+  const [openViewModal, setOpenViewModal] = useState(false); // State for ViewLead modal
 
-const UserLists = () => {
-  const { userlgs, dispatch: dispatchUsers } = useUsersContext()
+  const handleClick = async (userId) => {
+    if (!userLG) {
+      return;
+    }
+    setSelectedUserId(userId);
+    setShowConfirmation(true);
+  };
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch('/api/userLG')
-      const json = await response.json()
+  const handleOpenAssignModal = (userId) => {
+    setSelectedUserId(userId);
+    setOpenAssignModal(true);
+  };
+
+  const handleCloseAssignModal = () => {
+    setOpenAssignModal(false);
+    setSelectedUserId(null);
+  };
+
+  const handleDeleteConfirmation = async () => {
+    try {
+      setLoadingDelete(true); // Start delete loading
+      const response = await fetch(`/api/leads/${selectedUserId}`, {
+        method: "DELETE",
+        headers: {
+          'Authorization': `Bearer ${userLG.token}`
+        }
+      });
+      const json = await response.json();
 
       if (response.ok) {
-        dispatchUsers({ type: 'SET_USERS', payload: json })
+        dispatch({ type: "DELETE_USER", payload: json });
+        setSnackbarMessage("User Deleted Successfully!");
+        setSnackbarOpen(true);
+        onUserUpdate();
       }
+    } catch (error) {
+      setErrorDelete('Error deleting user.'); // Set delete error
+      console.error('Error deleting user:', error);
+    } finally {
+      setLoadingDelete(false); // Stop delete loading
+      setShowConfirmation(false); // Close confirmation dialog
     }
+  };
 
-    fetchUsers()
-  }, [dispatchUsers])
+  const handleCloseConfirmation = () => {
+    setShowConfirmation(false); // Close confirmation dialog
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handleOpenViewModal = (userId) => {
+    setSelectedUserId(userId);
+    setOpenViewModal(true);
+  };
+
+  const handleCloseViewModal = () => {
+    setOpenViewModal(false);
+    setSelectedUserId(null);
+  };
+
+  const iconButtonStyle = { color: "#e0e0e0" };
 
   // Custom rendering function for boolean values
   const renderBooleanCell = (params) => {
@@ -50,109 +114,256 @@ const UserLists = () => {
     );
   };
 
-  const columns = useMemo(() => [
+  const columns = [
     {
-      field: 'name',
-      headerName: 'Name',
-      width: 140,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400'
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+      minWidth: 120,
+      renderCell: (params) => params.value.slice(20, 26)
     },
     {
-      field: 'email',
-      headerName: 'Email',
-      width: 210,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400'
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 200,
+      cellClassName: "name-column--cell",
     },
     {
-      field: 'role',
-      headerName: 'Role',
-      width: 160,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400'
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      minWidth: 250,
     },
     {
-      field: 'gender',
-      headerName: 'Gender',
-      width: 100,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400',
-      renderCell: renderGenderCell
+      field: "role",
+      headerName: "Role",
+      flex: 1,
+      minWidth: 220,
     },
     {
-      field: 'number',
-      headerName: 'Phone Number',
-      width: 150,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400',
-      sortable: false,
-      filterable: false
+      field: "gender",
+      headerName: "Gender",
+      flex: 1,
+      minWidth: 200,
+      renderCell: renderGenderCell,
     },
     {
-      field: 'birthday',
-      headerName: 'Birthday',
-      width: 130,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400',
+      field: "number",
+      headerName: "Phone Number",
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: "birthday",
+      headerName: "Birthday",
+      flex: 1,
+      minWidth: 180,
       renderCell: (params) =>
-        moment(params.row.birthday).format('MMM-D-YYYY'),
+        moment(params.row.birthday).format('YYYY-MM-DD'),
     },
     {
-      field: 'homeaddress',
-      headerName: 'Home Address',
-      width: 130,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400'
+      field: "isActive",
+      headerName: "Active",
+      flex: 1,
+      minWidth: 150,
+      renderCell: renderBooleanCell,
     },
     {
-      field: 'isActive',
-      headerName: 'Active',
-      width: 90,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400',
-      renderCell: renderBooleanCell
-    },
-    {
-      field: 'createdAt',
-      headerName: 'Created At',
-      width: 130,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400',
+      field: "createdAt",
+      headerName: "Created At",
+      flex: 1,
+      minWidth: 180,
       renderCell: (params) =>
         moment(params.row.createdAt).format('MMM-D-YYYY'),
     },
     {
-      field: '_id',
-      headerName: 'Id',
-      width: 230,
-      headerClassName: 'bg-blue-gray text-white',
-      cellClassName: 'bg-black-gray font-bold border-b border-gray-400'
+      field: "actions",
+      headerName: "Actions",
+      flex: 1,
+      minWidth: 200,
+      renderCell: (params) => (
+        <Box>
+          <IconButton onClick={() => handleOpenViewModal(params.row._id)} style={iconButtonStyle}><Visibility /></IconButton>
+          <IconButton onClick={() => handleClick(params.row._id)} style={iconButtonStyle}><Delete /></IconButton>
+          <IconButton onClick={() => handleOpenAssignModal(params.row._id)} style={iconButtonStyle}><Edit /></IconButton>
+        </Box>
+      )
     },
-  ], [])
+  ];
 
   return (
-    <Box
-      sx={{
-        height: 450,
-        width: '100%'
-      }}
-    >
-      <Typography
-        variant='h3'
-        component='h3'
-        sx={{ textAlign: 'center', mt: 3, mb: 3 }}
+    <Box m="20px">
+      <Header
+        title="Chromagen Staffs"
+        subtitle="List of Users"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+            color: "#e0e0e0",
+            borderTop: "1px solid #525252",
+          },
+          "& .name-column--cell": {
+            color: "#94e2cd",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: "#3e4396",
+            borderBottom: "none",
+            color: "#e0e0e0",
+            fontSize: "18px",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "#101624",
+            fontSize: "17px",
+          },
+          "& .MuiDataGrid-headerContainer": {
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: "#3e4396",
+          },
+          "& .MuiCheckbox-root": {
+            color: `#b7ebde !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `#e0e0e0 !important`,
+          },
+        }}
       >
-        Manage Users
-        </Typography>
-      <DataGrid
-        columns={columns}
-        rows={userlgs}
-        getRowId={row => row._id}
-        className="bg-blue-gray"
+        <DataGrid
+          rows={userlgs}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { pageSize: 25, page: 0 },
+            },
+          }}
+          checkboxSelection
+          onSelectionModelChange={(newSelection) => {
+            setSelectedRows(newSelection);
+          }}
+          selectionModel={selectedRows}
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          getRowId={row => row._id}
+        />
+      </Box>
+      <Modal
+        open={openAssignModal}
+        onClose={handleCloseAssignModal}
+        aria-labelledby="assign-lead-modal-title"
+        aria-describedby="assign-lead-modal-description"
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          {selectedUserId && <EditUserInfo userId={selectedUserId} onUserUpdate={onUserUpdate} />}
+        </Box>
+      </Modal>
+      <Modal
+        open={loadingDelete}
+        onClose={() => setLoadingDelete(false)}
+        aria-labelledby="delete-lead-modal-title"
+        aria-describedby="delete-lead-modal-description"
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+            textAlign: 'center',
+          }}
+        >
+          {loadingDelete ? (
+            <CircularProgress /> // Show CircularProgress while deleting
+          ) : (
+              <div>{errorDelete || 'User Deleted Successfully!'}</div> // Show error or success message
+            )}
+        </Box>
+      </Modal>
+      <Modal
+        open={showConfirmation}
+        onClose={handleCloseConfirmation}
+        aria-labelledby="delete-confirmation-modal-title"
+        aria-describedby="delete-confirmation-modal-description"
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+            textAlign: 'center',
+          }}
+        >
+          <div>Are you sure you want to delete this user?</div>
+          <Button onClick={handleDeleteConfirmation}>Yes</Button>
+          <Button onClick={handleCloseConfirmation}>No</Button>
+        </Box>
+      </Modal>
+      <Modal
+        open={openViewModal}
+        onClose={handleCloseViewModal}
+        aria-labelledby="view-lead-modal-title"
+        aria-describedby="view-lead-modal-description"
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '80%',
+            maxHeight: '80%',
+            overflow: 'auto',
+          }}
+        >
+          {selectedUserId && <ReadUserInfo userId={selectedUserId} />}
+        </Box>
+      </Modal>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
+        action={<CheckCircleIcon sx={{ color: '#94e2cd' }} />}
       />
     </Box>
-  )
-}
+  );
+};
 
-export default UserLists
+export default UserLists;
